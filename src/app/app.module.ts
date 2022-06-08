d73bf0a686b4f6c5e4a97595103108b0f82d994b
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-08 19:15:46 +0200

    adding universal (ssr)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
   enabled: environment.production,
   // Register the ServiceWorker as soon as the application is stable
   // or after 30 seconds (whichever comes first).
