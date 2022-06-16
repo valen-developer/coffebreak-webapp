44d9a751b2209ecdf3ba33954b252a68c91795e5
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-17 01:40:08 +0200

    building profile page

--- src/app/presentation/public/pages/settings/pages/profile/profile.component.ts
+++ src/app/presentation/public/pages/settings/pages/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthStatusService } from 'src/app/application/Auth/AuthStatus.service';
+import { PlaylistFinder } from 'src/app/application/Playlist/PlaylistFinder';
 import { ImageGetter } from 'src/app/application/Shared/ImageGetter';
+import { Playlist } from 'src/app/domain/Playlist/Playlist.model';
 import { Nullable } from 'src/app/domain/Shared/types/Nullable.type';
 import { User } from 'src/app/domain/User/User.mode';
+import { asyncMap } from 'src/app/helpers/asyncMap';
 
 @Component({
   templateUrl: './profile.component.html',
@@ -14,8 +17,12 @@ export class ProfileComponent implements OnInit, OnDestroy {
   public userImage: Nullable<string>;
   private userSubscription!: Subscription;
 
+  private playlists: Playlist[] = [];
+  public playlistsData: PlaylistData[] = [];
+
   constructor(
     private authStatus: AuthStatusService,
+    private playlistFinder: PlaylistFinder,
     private imageGetter: ImageGetter
   ) {}
 
@@ -27,17 +34,41 @@ export class ProfileComponent implements OnInit, OnDestroy {
     this.userSubscription?.unsubscribe();
   }
 
+  private async getPlaylists(): Promise<void> {
+    if (!this.user) return;
+    this.playlists = await this.playlistFinder.getPlayListByOwner(
+      this.user?.uuid.value
+    );
+
+    this.playlistsData = await asyncMap(this.playlists, async (playlist) => {
+      const image = await this.getImage(playlist.uuid.value);
+
+      return {
+        name: playlist.name.value,
+        episodes: playlist.getEpisodesCount(),
+        imageUrl: image ?? 'https://via.placeholder.com/150',
+      };
+    });
+  }
+
   private userSubscriber(): void {
     this.userSubscription = this.authStatus.user$.subscribe({
       next: async (user) => {
         if (!user) return;
         this.user = user;
-        this.userImage = await this.getImage(user);
+        this.userImage = await this.getImage(user.uuid.value);
+        this.getPlaylists();
       },
     });
   }
 
-  private async getImage(user: User): Promise<Nullable<string>> {
-    return this.imageGetter.getDataUrlFromEntity(user.uuid.value);
+  private async getImage(uuid: string): Promise<Nullable<string>> {
+    return this.imageGetter.getDataUrlFromEntity(uuid);
   }
+}
+
+interface PlaylistData {
+  name: string;
+  episodes: number;
+  imageUrl: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.17:3000/api',
-  socketUrl: 'ws://192.168.0.17:3000',
+  apiUrl: 'http://192.168.0.21:3000/api',
+  socketUrl: 'ws://192.168.0.21:3000',
 };
 
 /*
