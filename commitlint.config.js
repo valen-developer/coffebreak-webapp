b1cec3f29556235ba1dcad99b4edccef35ba931e
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-26 03:05:01 +0200

    fix($config): set types and scope for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-angular']}
+module.exports = {
+  extends: ["@commitlint/config-angular"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+  },
+};
