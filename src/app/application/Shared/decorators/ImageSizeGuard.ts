309878b1109944b3e444d4415753b13c63482383
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-26 18:41:59 +0200

    feat($application): image size guard decorator

---
+++ src/app/application/Shared/decorators/ImageSizeGuard.ts
@@ -0,0 +1,35 @@
+import {
+  byteUnitsConverter,
+  stringInBytesConverter,
+} from 'src/app/helpers/byteUnitsConverter';
+
+export const ImageSizeGuard = () => {
+  const IMAGE_SIZE_LIMIT = 2 * 1000 * 1000;
+  const bytes = byteUnitsConverter({
+    quantity: IMAGE_SIZE_LIMIT,
+    unit: 'Bytes',
+    targetUnit: 'Bytes',
+  });
+
+  const message = stringInBytesConverter(bytes);
+
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    // the target method receive a image params which is a File object. We need to check if the image size is less than the max size allowed.
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      const object: { image?: File } = args[0];
+      const image = object.image;
+
+      if (!image) return originalMethod.apply(this, args);
+
+      console.log(stringInBytesConverter(image.size));
+
+      if (image.size > IMAGE_SIZE_LIMIT) {
+        throw new Error(`La imagen debe ser menor a ${message}`);
+      }
+
+      return originalMethod.apply(this, args);
+    };
+  };
+};
