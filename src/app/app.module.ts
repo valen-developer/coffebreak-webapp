d39c8292cac920e728d18bcae1c03c0d56d73b59
Valentin Valverde <valenreta.developer@gmail.com>
2022-08-24 19:45:59 +0200

    feat($infrastructure): adding analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { environment } from '../environments/environment';
 import { AlertModule } from './presentation/shared/modules/alert/alert.module';
 import { RouteReuseStrategy } from '@angular/router';
 import { CustomReuseStrategy } from './infrastructure/Shared/CustomReuseStrategy';
+import {
+  NgxGoogleAnalyticsModule,
+  NgxGoogleAnalyticsRouterModule,
+} from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,13 +22,15 @@ import { CustomReuseStrategy } from './infrastructure/Shared/CustomReuseStrategy
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
+    AlertModule,
+    NgxGoogleAnalyticsModule.forRoot('G-F3830WZ0F4'),
+    NgxGoogleAnalyticsRouterModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    AlertModule,
   ],
   providers: [
     ...providers,
